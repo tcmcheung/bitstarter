29d161e8ebb1a6dcc26d66db6f23cf0ff2906bad
Travis Cheung <travis.cheung@mail.utoronto.ca>
2013-07-11 07:35:05 +0000

    test

--- web.js
+++ web.js
@@ -3,10 +3,13 @@
 
 var express = require('express');
 var file = require('fs');
+var filename = new Buffer (64);
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World from'+file.readFile);
+      
+ response.send('Hello World from  '+filename);
 });
 
 var port = process.env.PORT || 5000;
