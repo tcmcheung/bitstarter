c1666a07ffe35724d2062d03010f9d6519fbb5d6
Travis Cheung <travis.cheung@mail.utoronto.ca>
2013-07-11 09:41:42 +0000

    Modified buffer size determination

--- web.js
+++ web.js
@@ -2,7 +2,7 @@
 
 var express = require('express');
 var file = require('fs');
-var welcomemsg = new Buffer (fs.readFileSync('index.html'));
+var welcomemsg = new Buffer (fs.readFileSync('index.html'),'utf-8');
 var fs = require ('fs');
 
 var app = express.createServer(express.logger());
